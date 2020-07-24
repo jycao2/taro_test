3a0b7be08890f2ba91a4999f3620b56ce69e52c1
jycao <1129251361@qq.com>
2020-07-24 17:03:42 +0800

    引入taro-ui

---
+++ README.md
@@ -0,0 +1,3 @@
+## 学习 taro
+
+目前 taro3.0 和 taroui 2.x 不兼容 使用 npm install taro-ui@3.0.0-alpha.3
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,23 +1,41 @@
 import Nerv, { Component } from 'nervjs'
+import { AtNoticebar, AtSearchBar, AtIcon } from 'taro-ui'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      value: ''
+    }
+  }
 
-  componentWillMount () { }
+  onChange(value) {
+    this.setState({
+      value: value
+    }, () => {
+      console.log(this.state.value)
+    })
+  }
+  componentWillMount() { }
 
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  render () {
+  render() {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <AtNoticebar close icon='volume-plus'>马上快要下班了</AtNoticebar>
+        <AtSearchBar
+          value={this.state.value}
+          onChange={this.onChange.bind(this)}
+        />
       </View>
     )
   }
